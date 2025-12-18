69fec6e45f6dfbd772b6da17b035b32740070be7
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-18 14:53:37 +0100

    es. 7

--- script7.js
+++ script7.js
@@ -20,4 +20,10 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+const studenteId2 = students.find((student) => {
+  return student.id === 2
+})
+
+console.log(studenteId2)
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
