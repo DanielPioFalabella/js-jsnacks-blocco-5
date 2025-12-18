a4ee705dd7081c10bf8cabe27fc6e1ff5c14cf94
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-18 14:46:53 +0100

    es. 5

--- script5.js
+++ script5.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const numeriPari = nums.filter((numero) => {
+    return numero % 2 === 0
+})
+
+console.log(numeriPari)
+
 // Risultato: [2, 8, 4, 12]
